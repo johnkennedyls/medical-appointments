790fe878014f9ca5146858fc45fb395dbd86e87d
johnkennedyls <jkls1998@gmail.com>
2025-06-28 13:57:59 -0500

    get and setters

--- medicalApointments/src/Main.java
+++ medicalApointments/src/Main.java
@@ -7,6 +7,18 @@ public class Main {
         System.out.println("Doctor Name: " + myDoctor.name);
         System.out.println("Doctor Specialty: " + myDoctor.specialty);
 
+        Patient patient = new Patient("Carl", "carl123@gmail.com");
+        System.out.println("Patient Name: " + patient.getName());
+        System.out.println("Patient Email: " + patient.getEmail());
+
+        patient.setWeight(70.5);
+        patient.setHeight(1.75);
+        System.out.println("Patient Weight: " + patient.getWeight());
+        System.out.println("Patient Height: " + patient.getHeight());
+
+        patient.setPhoneNumber("1234567890");
+        System.out.println("Patient Phone Number: " + patient.getPhoneNumber());
+
 
         //showMenu();
 
--- medicalApointments/src/Patient.java
+++ medicalApointments/src/Patient.java
@@ -1,26 +1,96 @@
 public class Patient {
-    public String name;
-    public String email;
-    public String address;
-    public String phoneNumber;
-    public String birthDay;
-    public String weight;
-    public String height;
-    public String blood;
+
+    private int id;
+    private String name;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String birthDay;
+    private double weight;
+    private double height;
+    private String blood;
 
     public Patient(String name, String email){
         this.name = name;
         this.email = email;
     }
 
-    public Patient(String name, String email, String address, String phoneNumber, String birthDay, String weight, String height) {
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
-        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber.length() > 10) {
+            System.out.println("The phone number is too long");
+            return;
+        } else if (phoneNumber.length() == 10) {
+            this.phoneNumber = phoneNumber;
+
+        }
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
         this.birthDay = birthDay;
+    }
+
+    public String getWeight() {
+        return weight + " kg";
+    }
+
+    public void setWeight(double weight) {
         this.weight = weight;
+    }
+
+    public String getHeight() {
+        return height + " m";
+    }
+
+    public void setHeight(double height) {
         this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
     }
 }
 
